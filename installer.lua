769fdeae826522b38113c9f0ec69958f8c13ed94
UAGoose <arthur.zuiev@gmail.com>
2025-03-03 13:15:49 +0100

    nother test

--- installer.lua
+++ installer.lua
@@ -1,144 +1,90 @@
+-- Required Libraries
 local component = require("component")
 local internet = require("internet")
 local fs = require("filesystem")
 local shell = require("shell")
 local json = nil
 
+-- Configuration
 local config = {
     git = {
-        name = "arthurzuiev",
-        repo = "OS"
+        owner = "arthurzuiev",  -- GitHub username
+        repo = "OS"             -- Repository name
     }
 }
 
-local gitbaseURL = "https://api.github.com/repos/"
-
--- constants
-local NO_RESOURCE_ERR = 6
+-- Constants
+local NO_INTERNET_ERR = 6
 local GENERAL_ERR = 1
 
--- utility
+-- Utility Functions
 local function throwError(msg, errCode)
-    error(msg)
+    print(msg)
     os.exit(errCode)
 end
 
 local function installJSON()
-    shell.execute('wget -fq "https://raw.githubusercontent.com/rater193/OpenComputers-1.7.10-Base-Monitor/master/lib/json.lua" "/lib/json.lua"')
+    if not fs.exists("/lib/json.lua") then
+        shell.execute('wget -fq "https://raw.githubusercontent.com/rater193/OpenComputers-1.7.10-Base-Monitor/master/lib/json.lua" "/lib/json.lua"')
+    end
     json = require("json")
 end
 
-local function confirmInternetAcess()
-    -- in case it is used on floppy discs
-    local internetCard = component.isAvailable("internet")
-    if internetCard == false then
-        print("No internet acess detected.")
-        print("Aborting installation.")
-
-        throwError("No internet acess detected.", NO_RESOURCE_ERR)
-        return false
+local function confirmInternetAccess()
+    if not component.isAvailable("internet") then
+        throwError("No internet access detected.", NO_INTERNET_ERR)
     end
-
-    return true
 end
 
--- core functionality 
-local function stringifyResponse(response)
-    local res = ""
-    local resp = response
-
-    while(resp~=nil) do
-        res = ret..tostring(resp)
-        resp = responce
-    end
-
-    return res
-end
-
-local function httpRequst(url)
-    local stringifiedResponse = nil
-
+local function httpRequest(url)
     local success, response = internet.request(url)
-
-    print(sucess, repsonse)
-
-    return response
+    if not success then
+        throwError("Failed to fetch data from: " .. url, GENERAL_ERR)
+    end
+    local data = ""
+    for chunk in response do
+        data = data .. chunk
+    end
+    return data
 end
 
-local function downloadRepoTree(treeDataURL, parentdir)
-    if(not parentdir) then parentdir = "" end
-
-    local treeData = json.decode(httpRequst(treeDataURL))
-
-    for _, child in pairs(treedata.tree) do
-        --os.sleep(0.1) TODO: check if this is necessary
-        local filename = parentdir.."/"..tostring(child.path)
-
-        if(child.type=="tree") then
+local function downloadRepoTree(treeDataURL, parentDir)
+    parentDir = parentDir or ""
+    local treeData = json.decode(httpRequest(treeDataURL))
+    for _, child in ipairs(treeData.tree) do
+        local filename = parentDir .. "/" .. child.path
+        if child.type == "tree" then
             downloadRepoTree(child.url, filename)
         else
-            shell.execute('rm -f "'..tostring(filename)..'"')
-            local repodata = httpRequst("https://raw.githubusercontent.com/"..tostring(config.git.name).."/"..tostring(config.git.repo).."/master/"..tostring(filename))
+            shell.execute('rm -f "' .. filename .. '"')
+            local fileData = httpRequest("https://raw.githubusercontent.com/" .. config.git.owner .. "/" .. config.git.repo .. "/main/" .. filename)
             local file = fs.open(filename, "w")
-            file:write(repodata)
+            file:write(fileData)
             file:close()
         end
     end
 end
 
-local function getTreedataURL()
-    local mainData = httpRequst(gitbaseURL..tostring(config.git.name).."/"..tostring(config.git.repo).."/git/refs")
-
-    if (mainData) then
-        local git = json.decode(data)[1].object
-        local gitsha = git.sha
-        local giturl = git.url
-
-        local commitdata = httpRequst(gitcommit)
-
-        if (commitdata) then 
-            local commitdatatree = json.decode(commitdata).tree
-
-            return commitdatatree.url
-        else
-            throwError("Failed to get commit data.", GENERAL_ERR)
-        end
-    else
-        throwError("Failed to get tree data.", GENERAL_ERR)
-    end
+local function getTreeDataURL()
+    local refsData = httpRequest("https://api.github.com/repos/" .. config.git.owner .. "/" .. config.git.repo .. "/git/refs/heads/main")
+    local refs = json.decode(refsData)
+    local commitData = httpRequest(refs.object.url)
+    local commit = json.decode(commitData)
+    return commit.tree.url
 end
 
--- init functionality 
-local function runInit()
-    -- running the init.lua that was installed in tge process
-    shell.execute("init.lua")
-end
-
+-- Installation Process
 local function install()
-    print("Installer started.")
-    print()
-
-    print("--Preparation...")
-    -----------------------
-    confirmInternetAcess()
+    print("Installer started.\n")
+    confirmInternetAccess()
     installJSON()
-    -----------------------
-    print("--Preparation end.")
+    print("Preparation complete.\n")
 
-
-    print()
-
-
-    print("Intallation initiated.")
-    -----------------------
-    local treeDataURL = getTreedataURL()
+    print("Installation initiated.")
+    local treeDataURL = getTreeDataURL()
     downloadRepoTree(treeDataURL)
-    -----------------------
-    print("Installation complete.")
-
-    -- now run all installed shit
-    shell.execute("cls")
-    runInit()
+    print("Installation complete.\n")
 end
 
+-- Run Installer
 install()

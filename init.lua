a5111ac0561007e5d611f3809bd1b9fb37405b2d
UAGoose <arthur.zuiev@gmail.com>
2025-03-03 14:37:39 +0100

    makinf bootloader thingy.... IT IS HELL... there is no require or dofile in there

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
-local bootloader = dofile("/bootloader/bootloader.lua")
+local function loadScript(path)
+    local file = io.open(path, "r")
+    if file then
+        local content = file:read("*a")
+        file:close()
+        local func, err = load(content, path)
+        if func then
+            func()
+        else
+            print("Error loading script: " .. err)
+        end
+    else
+        print("File not found: " .. path)
+    end
+end
+
+local bootloader = loadScript("/bootloader/bootloader.lua")
 bootloader.load()

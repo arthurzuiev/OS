0759095dae3657190df36ebc7c9db63bbf28ca71
UAGoose <arthur.zuiev@gmail.com>
2025-03-03 16:54:54 +0100

    test

--- init.lua
+++ init.lua
@@ -7,3 +7,8 @@ gpu.setResolution(80, 25)  -- Example: 80 characters wide, 25 rows tall
 
 -- Display text at the top-left corner (0, 0)
 gpu.set(1, 1, "Hello from BIOS!")
+
+-- dont halt it
+while true do
+  computer.pullSignal()
+end

2e7bde396e6aed7d16311180f946e0d0c3b5d2ec
UAGoose <arthur.zuiev@gmail.com>
2025-03-03 16:50:49 +0100

    clear doesnt work for some reason...

--- init.lua
+++ init.lua
@@ -5,8 +5,5 @@ local gpu = _G.component.proxy(component.list("gpu")())
 -- Set the screen resolution (optional)
 gpu.setResolution(80, 25)  -- Example: 80 characters wide, 25 rows tall
 
--- Clear the screen
-gpu.clear()
-
 -- Display text at the top-left corner (0, 0)
 gpu.set(1, 1, "Hello from BIOS!")
